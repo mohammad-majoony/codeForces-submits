48726d0e2e78aa18293f0333aa15c26fb1c460eb
mmd <mohmad1382m@gmail.com>
2023-06-03 00:52:04 +0430

    WLC CPP

---
+++ CPP/Watermelon.cpp
@@ -0,0 +1,13 @@
+#include<iostream> 
+
+using namespace std ;
+
+int main(){
+    int num ;
+    cin >> num ;
+    if(num % 2 == 0 && num != 2)
+        cout << "YES" ;
+    else 
+        cout << "NO" ;
+    return 0 ;
+}
